28cbf17212141fd2037a1c5a6255944a86fda48f
Jason Thorpe <jason.thorpe@utel.co.uk>
2024-10-02 15:09:08 +0100

    Updated code

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-This is a test of openJDK 21
+# Hello World Java Application
+
+This is a simple Java application demonstrating basic output and object manipulation using Microsoft OpenJDK 21.
+
+## Features
+
+- Prints "Hello World!" to the console.
+- Tests Microsoft OpenJDK 21.
+- Creates and prints `ResultData` and `LoginResult` objects.
+- Outputs `LoginResult` in JSON format using `ToStringBuilder`.
+
+## Prerequisites
+
+- Java Development Kit (JDK) 21
+- Apache Commons Lang library (for `ToStringBuilder`)
+
+## Getting Started
+
+1. **Clone the repository:**
+
+    ```sh
+    git clone https://github.com/yourusername/helloworld-java.git
+    cd helloworld-java
+    ```
+
+2. **Compile the code:**
+
+    ```sh
+    javac -cp .:commons-lang3-3.12.0.jar uk/co/utel/test/UtelHelloworld.java
+    ```
+
+3. **Run the application:**
+
+    ```sh
+    java -cp .:commons-lang3-3.12.0.jar uk.co/utel/test/UtelHelloworld
+    ```
+
+## Code Overview
+
+```java
+public class UtelHelloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("This is a test of Microsoft OpenJDK 21");
+
+        System.out.println("Make some output data");
+        ResultData rd = new ResultData(1, "Hello");
+        System.out.println("rd=" + rd);
+
+        LoginResult lr = new LoginResult();
+        lr.setId(123L);
+        lr.setFirstName("Jason");
+        lr.setLastName("Thorpe");
+
+        System.out.println("uk.co.utel.test.UtelHelloworld.main(): " + lr.toString());
+
+        String jsonLR = ToStringBuilder.reflectionToString(lr, ToStringStyle.JSON_STYLE);
+        System.out.println("uk.co.utel.test.UtelHelloworld.main(): " + jsonLR);
+    }
+}
--- src/main/java/uk/co/utel/test/UtelHelloworld.java
+++ src/main/java/uk/co/utel/test/UtelHelloworld.java
@@ -1,7 +1,29 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Copyright (c) 2024 Jason Thorpe
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
+
 package uk.co.utel.test;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 /**
  *
@@ -21,7 +43,17 @@ public class UtelHelloworld {
         System.out.println("Make some output data");
         ResultData rd = new ResultData(1, "Hello");
         System.out.println("rd=" + rd);
+        
+        
+        LoginResult lr= new LoginResult();
+        lr.setId(123L);
+        lr.setFirstName("Jason");
+        lr.setLastName("Thorpe");
+        
+        System.out.println("uk.co.utel.test.UtelHelloworld.main(): " + lr.toString());
 
+       String  jsonLR= ToStringBuilder.reflectionToString(lr, ToStringStyle.JSON_STYLE);
+       System.out.println("uk.co.utel.test.UtelHelloworld.main(): " + jsonLR);
         
     }
 }
